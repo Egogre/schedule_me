2d95bec483b42a011e215a2c2e10538c61a2005a
George Hudson <georgehudson78@gmail.com>
2016-01-15 13:00:14 -0700

    created user model test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "valid user with valid attributes" do
+    user = User.new(first_name: "Test",
+                    last_name: "User",
+                    email: "testuser@mail.com",
+                    password: "password")
+
+    assert user.valid?
+  end
+
+  test "invalid user without valid attributes" do
+    user1 = User.new(last_name: "User",
+                    email: "testuser@mail.com",
+                    password: "password")
+
+    user2 = User.new(first_name: "Test",
+                    email: "testuser@mail.com",
+                    password: "password")
+
+    user3 = User.new(first_name: "Test",
+                    last_name: "User",
+                    password: "password")
+
+    user4 = User.new(first_name: "Test",
+                    last_name: "User",
+                    email: "testuser@mail.com")
+
+    refute user1.valid?
+    refute user2.valid?
+    refute user3.valid?
+    refute user4.valid?
+  end
+
 end
